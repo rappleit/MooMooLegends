5f965d24883d6c85b0e209cb0c8308f74ba9cd35
EMKwasam <samuelkwa1@gmail.com>
2024-06-10 11:03:04 +0800

    Completed page  to enter room code
    Completed joining_private_room.xml and added additional assets to the drawable folder

--- app/src/main/java/com/csd/moomoolegends/MainActivity.java
+++ app/src/main/java/com/csd/moomoolegends/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_has_not_joined_room);
+        setContentView(R.layout.activity_home);
     }
 }
