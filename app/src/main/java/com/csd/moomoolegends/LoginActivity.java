3c59e96246351f8f240bd73631d08b59060cc368
mikexql <98818517+mikexql@users.noreply.github.com>
2024-06-12 16:44:22 +0800

    login page

---
+++ app/src/main/java/com/csd/moomoolegends/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.csd.moomoolegends;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.csd.moomoolegends.models.OnFirestoreCompleteCallback;
+import com.csd.moomoolegends.models.SignUpLoginFirestore;
+import com.csd.moomoolegends.models.UserFirestore;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout usernameText;
+    private TextInputLayout passwordText;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+        TextInputLayout usernameText = findViewById(R.id.sign_in_username);
+        TextInputLayout passwordText = findViewById(R.id.sign_in_password);
+        ImageButton signIn=findViewById(R.id.sign_in_butt);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getEditText().getText().toString().trim();
+                String password = passwordText.getEditText().getText().toString().trim();
+                Log.d("login", username+password);
+                SignUpLoginFirestore.getInstance().login(LoginActivity.this, username, password, new OnFirestoreCompleteCallback() {
+                    @Override
+                    public void onFirestoreComplete(boolean success, String message) {
+                        if (success){
+                            Log.d("Login", message);
+
+                        } else {
+                            Log.d("Login", message);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
