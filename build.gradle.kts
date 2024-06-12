a4386d17ca093551f87c5d91a4a9ecb5e6181cde
Rachel <rachel.limre03@gmail.com>
2024-06-13 02:50:22 +0800

    Fixed build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -8,11 +10,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-}
-
-buildscript {
-    dependencies {
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
 }
 
